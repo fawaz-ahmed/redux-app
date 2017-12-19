0bda8e1381d914cbd4460928181e80db38319f88
Fawaz Ahmed <fawaz_ahmed@live.com>
2017-12-20 02:37:07 +0500

    added users list. setup app with redux and reduc thunk.

---
+++ src/components/App.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import usersActions from '../actions/users'
+//import { Container, Row, Col } from 'reactstrap';
+
+const { loadUsers } = usersActions;
+
+class App extends Component {
+
+  componentWillMount() {
+    this.props.loadUsers();
+  }
+  render() {
+    const { users } = this.props;
+
+    const parentStyle = {
+      margin: 20,
+    };
+
+    const itemStyle = {
+      cursor: 'pointer',
+      borderRadius: 10,
+      padding: 10,
+      border: '2px solid lightgrey',
+      marginBottom: 5,
+      color: 'darkgrey',
+    };
+
+    return (
+      <div className="App" style={parentStyle}>
+        {users.map(user=> (
+          <div key={`user-${user.id}`} style={itemStyle}>
+            {user.login}<br />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {...state.users};
+}
+
+export default connect(mapStateToProps, {
+  loadUsers
+})(App);
