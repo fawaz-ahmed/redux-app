3ae59f382f2a827c0b3e483c9071d0bf3feb21fb
Fawaz Ahmed <fawaz_ahmed@live.com>
2017-12-20 02:46:31 +0500

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# redux-app
-React-redux sample application
--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import users from './users';
-
-const rootReducer = combineReducers({
-  users,
-  // add more reducers here
-})
-
-export default rootReducer;
