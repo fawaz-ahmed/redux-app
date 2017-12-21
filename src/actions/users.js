2ebf31a7b628cf336900b076ff69b3bfe43f9645
Fawaz Ahmed <fawaz_ahmed@live.com>
2017-12-22 01:03:48 +0500

    fix fetch

--- src/actions/users.js
+++ src/actions/users.js
@@ -2,13 +2,15 @@ const usersActions = {
   LOAD_USERS: 'LOAD_USERS',
   loadUsers: () => {
     return (dispatch) => {
-      return fetch('https://api.github.com/users').then(response => {
-        setTimeout(() => {
-          dispatch({
-            type: usersActions.LOAD_USERS,
-            users: response.json()
-          });
-        }, 3000);
+      fetch('https://api.github.com/users').then(response => {
+        response.json().then(users => {
+          setTimeout(() => {
+            dispatch({
+              type: usersActions.LOAD_USERS,
+              users
+            });
+          }, 2000);
+        });
       });
     };
   },
