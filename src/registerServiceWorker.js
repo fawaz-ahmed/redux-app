523e38f71b89aeb20a6322310bce05e906acb8b2
Fawaz Ahmed <fawaz_ahmed@live.com>
2017-12-24 17:56:50 +0500

    update index.html,manifest and service worker scope settings.

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -45,9 +45,7 @@ export default function register() {
 
 function registerValidSW(swUrl) {
   navigator.serviceWorker
-    .register(swUrl, {
-      scope: '/redux-app/'
-    }).then(registration => {
+    .register(swUrl).then(registration => {
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
         installingWorker.onstatechange = () => {
